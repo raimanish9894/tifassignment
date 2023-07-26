9a7dcc5f68f411c7d775e6d3eda09c3a3b7fc5d9
raimanish9894 <raimanish9894@gmail.com>
2023-07-26 13:13:09 +0530

    created repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './component/About';
+import Carousel from './component/Carousel';
+import Footer from './component/Footer';
+import HeroSection from './component/HeroSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeroSection />
+      <About />
+      <Carousel />
+      <Footer />
     </div>
   );
 }
---
+++ src/component/CardCarousel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardCarousel = () => {
+  return (
+    <div>CardCarousel</div>
+  )
+}
+
+export default CardCarousel
